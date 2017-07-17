3b7ceac52e1877237f0019a584f78dbe94158bb4
JASON W HALSEY <jasonwhalsey@JASONs-MacBook-Pro.local>
2017-07-17 14:28:51 -0700

    add .each to jquery

--- js/napsa_custom.js
+++ js/napsa_custom.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function() {
 
 // Add Span to first word of Hero Title for Styling
 (function () { 
-    var node = jQuery("h2.et_pb_slide_title").contents().filter(function () { return this.nodeType == 3 }).first(),
+    var node = jQuery("h2.et_pb_slide_title").each().contents().filter(function () { return this.nodeType == 3 }).first(),
         text = node.text(),
         first = text.slice(0, text.indexOf(" "));
     
